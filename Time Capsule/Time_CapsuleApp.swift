2c1660772c03f836cbc44952a3186e1adbfb09d6
Mustafa Cakir <98mustafacan@gmail.com>
2024-01-20 22:21:35 +0300

    Initial Commit

---
+++ Time Capsule/Time_CapsuleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Time_CapsuleApp.swift
+//  Time Capsule
+//
+//  Created by MustafaCan on 20.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct Time_CapsuleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
